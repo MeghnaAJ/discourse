8621f002b5971905b4a41a271ae0a1690c473c79
Gerhard Schlager <gerhard.schlager@discourse.org>
2021-06-01 02:56:27 +0200

    WIP

--- app/models/trust_level_setting.rb
+++ app/models/trust_level_setting.rb
@@ -7,19 +7,24 @@ class TrustLevelSetting < EnumSiteSetting
     valid_values.any? { |v| v == val.to_i }
   end
 
-  # def self.values
-  #   levels = TrustLevel.levels
-  #   valid_values.map { |x|
-  #     {
-  #       name: x.is_a?(Integer) ? "#{x}: #{levels[x.to_i].name}" : x,
-  #       value: x
-  #     }
-  #   }
-  # end
+  def self.values
+    valid_values.map do |value|
+      { name: translation(value), value: value }
+    end
+  end
 
   def self.valid_values
     TrustLevel.valid_range.to_a
   end
 
+  def self.translation(value)
+    I18n.t(
+      "js.trust_levels.detailed_name",
+      level: value,
+      name: I18n.t("js.trust_levels.names.#{TrustLevel.levels[value]}")
+    )
+  end
+
   private_class_method :valid_values
+  private_class_method :translation
 end
